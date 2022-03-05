553a4711e858893712c0df5ad76e18cbd3ff48cb
Christian Rullan <laptopchris01@gmail.com>
2022-03-05 18:16:19 -0500

    completed sumPublishedPoints

--- src/nested.ts
+++ src/nested.ts
@@ -85,7 +85,11 @@ export function sumPoints(questions: Question[]): number {
  * Consumes an array of questions and returns the sum total of the PUBLISHED questions.
  */
 export function sumPublishedPoints(questions: Question[]): number {
-    return 0;
+    const publishArr: Question[] = questions.filter(
+        (question: Question): boolean => question.published === true
+    );
+    const totalPub: number = sumPoints(publishArr);
+    return totalPub;
 }
 
 /***
