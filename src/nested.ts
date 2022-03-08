3a191ca79c3d13095ff2fd1e09e33ae29f087bb1
Christian Rullan <laptopchris01@gmail.com>
2022-03-08 12:49:09 -0500

    second warning fix

--- src/nested.ts
+++ src/nested.ts
@@ -1,4 +1,3 @@
-import { isQuestion } from "./functions";
 import { Answer } from "./interfaces/answer";
 import { Question, QuestionType } from "./interfaces/question";
 import { duplicateQuestion, makeBlankQuestion } from "./objects";
