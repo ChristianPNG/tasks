eeccec9790eaa62ff83f6561ca4b958982073f75
Christian Rullan <laptopchris01@gmail.com>
2022-03-10 21:38:03 -0500

    completed RevealAnswer

--- src/components/RevealAnswer.tsx
+++ src/components/RevealAnswer.tsx
@@ -1,6 +1,23 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
+type QuizAnswer = 42 | "";
+
+const AnsMap: Record<QuizAnswer, QuizAnswer> = {
+    42: "",
+    "": 42
+};
+
 export function RevealAnswer(): JSX.Element {
-    return <div>Reveal Answer</div>;
+    function invertAns(): void {
+        const newAns: QuizAnswer = AnsMap[answer];
+        setAnswer(newAns);
+    }
+    const [answer, setAnswer] = useState<QuizAnswer>("");
+    return (
+        <div>
+            <Button onClick={invertAns}>Reveal Answer</Button>
+            {answer}
+        </div>
+    );
 }
